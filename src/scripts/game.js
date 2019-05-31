67787fb97a1effa42feaa43991bc0424aefdc9db
Cary <798171920@qq.com>
2019-05-31 18:42:36 +0800

    a

---
+++ src/scripts/game.js
@@ -0,0 +1,35 @@
+import 'phaser'
+import '@babel/polyfill'
+
+import MainScene from './scenes/mainScene'
+import PreloadScene from './scenes/preloadScene'
+
+const DEFAULT_WIDTH = 1024
+const DEFAULT_HEIGHT = 768
+
+const config = {
+  title: 'mygame',
+  backgroundColor: '#ffffff',
+  // scale: {
+  //   parent: 'phaser-game',
+  //   mode: Phaser.Scale.FIT,
+  //   autoCenter: Phaser.Scale.CENTER_BOTH,
+  //   width: DEFAULT_WIDTH,
+  //   height: DEFAULT_HEIGHT
+  // },
+  type: Phaser.AUTO,
+  width: DEFAULT_WIDTH,
+  height: DEFAULT_HEIGHT,
+  scene: [PreloadScene, MainScene],
+  // physics: {
+  //   default: 'arcade',
+  //   arcade: {
+  //     debug: false,
+  //     gravity: { y: 400 }
+  //   }
+  // }
+}
+
+window.addEventListener('load', () => {
+  let game = new Phaser.Game(config)
+})
